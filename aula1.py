4c8694dd4cd2dc1b4dcfadb843e38945aec003b5
EngineerGazon <nathangazon@hotmail.com>
2023-03-15 18:41:26 -0300

    first commit

---
+++ aula1.py
@@ -0,0 +1,19 @@
+class Pessoa:
+    def __init__(self, nome, sobrenome):
+
+        self.nome = nome
+        self.sobrenome = sobrenome
+
+
+p1 = Pessoa('Nathan', 'Gazon')
+# p1.nome = 'Nathan'
+# p1.sobrenome = 'Gazon'
+print(p1.nome)
+print(p1.sobrenome)
+print('')
+
+p2 = Pessoa('Maria', 'Elza')
+# p2.nome = 'Maria'
+# p2.sobrenome = 'Elza'
+print(p2.nome)
+print(p2.sobrenome)
