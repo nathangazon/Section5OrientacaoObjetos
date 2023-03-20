79720b55dcd9b809d21f5e085109c9e5e4c006a4
EngineerGazon <nathangazon@hotmail.com>
2023-03-19 22:55:18 -0300

    Nova_Mudancas

--- aula17.py
+++ aula17.py
@@ -9,8 +9,36 @@ class Cliente:
         self.enderecos = []
 
     def inserir_endereco(self, rua, numero):
-        self.enderecos.append()
+        self.enderecos.append(Endereco(rua, numero))
+
+    def listar_enderecos(self):
+        for endereco in self.enderecos:
+            print(endereco.rua, endereco.numero)
+
+    def inserir_endereco_externo(self, endereco):
+        self.enderecos.append(endereco)
+
+    def __del__(self):
+        print('APAGANDO', self.nome)
 
 
 class Endereco:
     def __init__(self, rua, numero):
+        self.rua = rua
+        self.numero = numero
+
+    def __del__(self):
+        print('APAGANDO, ', self.rua, self.numero)
+
+
+cliente1 = Cliente('Maria')
+cliente1.inserir_endereco('Av Brasil', 44)
+cliente1.inserir_endereco('Rua B', 3331)
+cliente1.listar_enderecos()
+endereco_externo = Endereco('Av Saudade', 11122)
+cliente1.inserir_endereco_externo(endereco_externo)
+
+del cliente1
+
+print(endereco_externo.rua, endereco_externo.numero)
+print('################Aqui termina o meu codigo')
